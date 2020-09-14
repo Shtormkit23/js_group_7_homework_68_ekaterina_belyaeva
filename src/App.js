20cce3daeb4c20bffe46e9befb654ad276b11ec6
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-09-14 18:04:14 +0600

    Добавлены роуты для переходам по главным контейнерам приложения

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Counter from "./containers/Counter/Counter";
+import Layout from "./components/Layout/Layout";
+import ToDoList from "./containers/ToDoList/ToDoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Switch>
+                <Route path="/" exact component={Counter} />
+                <Route path="/todo" component={ToDoList} />
+                <Route render={() => <h1>404 page not found</h1>} />
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
